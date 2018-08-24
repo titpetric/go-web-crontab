2e24746025f7384f348f193374552593d697f0fc
Tit Petric <black@scene-si.org>
2018-08-24 12:58:49 +0200

    import base db schema

---
+++ db/20180704080000.base.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `jobs` (
+ `name` varchar(64) NOT NULL,
+ `description` varchar(255) NOT NULL,
+ PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `logs` (
+ `name` varchar(64) NOT NULL,
+ `stamp` datetime NOT NULL,
+ `duration` bigint(20) NOT NULL,
+ PRIMARY KEY (`name`,`stamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
